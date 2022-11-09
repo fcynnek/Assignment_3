5171a7d7319ff9b0a9f5f48aa626e6376eb9e4c0
fcynnek <kchengfung@gmail.com>
2022-11-08 21:01:26 -0500

    Update on UserPOJO

--- src/com/fcynnek/assignment3/UserPOJO.java
+++ src/com/fcynnek/assignment3/UserPOJO.java
@@ -1,39 +1,28 @@
 package com.fcynnek.assignment3;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class UserPOJO {
 
+//	private String username;
+//	private String password;
+//	private String name;
+
 	BufferedReader fileReader = null;
 	
-	private String username;
-	private String password;
-	private String name;
+	String line;
 	
-	
-	
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	private void storingUserData () {
-		
-	}
 	try {
 		
 	}
+	fileReader = new BufferedReader(new FileReader("data.txt"));
+			
+		while ((line = fileReader.readLine()) != null) {
+			char[] line;
+			System.out.println(line);
+		}
+
+
 }
