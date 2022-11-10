bb5293c9e88b21a2fac21f13e45e479e59a0010b
fcynnek <kchengfung@gmail.com>
2022-11-09 20:19:15 -0500

    Creating file reader

--- src/com/fcynnek/assignment3/UserPOJO.java
+++ src/com/fcynnek/assignment3/UserPOJO.java
@@ -1,28 +1,45 @@
 package com.fcynnek.assignment3;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class UserPOJO {
+// this class will read a file, parse, and store user information
 
-//	private String username;
-//	private String password;
-//	private String name;
-
-	BufferedReader fileReader = null;
-	
-	String line;
-	
-	try {
-		
-	}
-	fileReader = new BufferedReader(new FileReader("data.txt"));
+		public BufferedReader fileReader () {
 			
-		while ((line = fileReader.readLine()) != null) {
-			char[] line;
-			System.out.println(line);
+			BufferedReader fileReader = null;
+			
+			try {
+				fileReader = new BufferedReader (new FileReader("data.txt"));
+				
+				String line;
+				try {
+					while ((line = fileReader.readLine()) != null) {
+						System.out.println(line);
+					}
+				} catch (IOException e) {
+					System.out.println("I/O Exception ocurred");
+					e.printStackTrace();
+				}
+			} catch (FileNotFoundException e) {
+				System.out.println(".txt File not found");
+				e.printStackTrace();
+			} finally {
+			// closing the file reader
+					try {
+						fileReader.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				
+			}
+			
+			
+			return fileReader;
+			
 		}
-
 
 }
